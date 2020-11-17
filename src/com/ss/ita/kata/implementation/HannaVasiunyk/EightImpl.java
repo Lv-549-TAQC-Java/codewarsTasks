a9d903d9d6c6327f925c89c5c40053567f3c07e4
Hanna Vasiunyk <annvasiunyk@gmail.com>
2020-11-17 18:53:54 +0200

    Added task Keep Hydrated

---
+++ src/com/ss/ita/kata/implementation/HannaVasiunyk/EightImpl.java
@@ -0,0 +1,14 @@
+package com.ss.ita.kata.implementation.HannaVasiunyk;
+
+import com.ss.ita.kata.Eight;
+
+public class EightImpl implements Eight {
+
+    @Override
+    public int Liters(double time) {
+        final double WATER = 0.5;
+        double count = time * WATER;
+        int intCount = (int) count;
+        return (intCount);
+    }
+}
