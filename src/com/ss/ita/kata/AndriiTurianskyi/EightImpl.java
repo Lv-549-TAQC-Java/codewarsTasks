e7c184f5e1e8a8642d7e5ca68434d39743e60799
AndriiTurianskyi <andriy.turyansky@gmail.com>
2020-11-17 22:28:56 +0200

    added an implementation method that returns the number of liters drunk, rounded to the lowest value

---
+++ src/com/ss/ita/kata/AndriiTurianskyi/EightImpl.java
@@ -0,0 +1,17 @@
+package com.ss.ita.kata.AndriiTurianskyi;
+
+import com.ss.ita.kata.Eight;
+
+public class EightImpl implements Eight {
+
+    @Override
+    public int Liters(double time) {
+        if (time >= 0) {
+            double literPerHour = 0.5;
+            double result = time * literPerHour;
+            return (int) result;
+        } else {
+            throw new IllegalArgumentException("time can't be less than 0");
+        }
+    }
+}
