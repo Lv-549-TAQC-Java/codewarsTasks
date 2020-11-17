aeca5d9dbfd06607fd785657a0db01221ae90a6c
Khrystyna Pelekh <Khrystyna.Pelekh@outlook.com>
2020-11-17 23:26:59 +0200

    To square(root) or not to square(root)
    Add implementation class ImplEight

---
+++ src/com/ss/ita/kata/implementation/Khrystyna/ImplEight.java
@@ -0,0 +1,18 @@
+package com.ss.ita.kata.implementation.Khrystyna;
+
+import com.ss.ita.kata.Eight;
+
+public class ImplEight implements Eight {
+    @Override
+    public int[] squareOrSquareRoot(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            double sqrt = Math.sqrt(array[i]);
+            if (sqrt % 1 == 0) {
+                array[i] = (int) sqrt;
+            } else {
+                array[i] *= array[i];
+            }
+        }
+        return array;
+    }
+}
