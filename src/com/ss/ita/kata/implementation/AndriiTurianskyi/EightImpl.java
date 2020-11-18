acbbe967c13031dcb03d1e70b21eb9d1c00900e5
AndriiTurianskyi <andriy.turyansky@gmail.com>
2020-11-18 21:41:43 +0200

    added to the EightImpl class the amIWilson method

---
+++ src/com/ss/ita/kata/implementation/AndriiTurianskyi/EightImpl.java
@@ -0,0 +1,61 @@
+package com.ss.ita.kata.implementation.AndriiTurianskyi;
+
+import com.ss.ita.kata.Eight;
+
+public class EightImpl implements Eight {
+    @Override
+    public int Liters(double time) {
+        return 0;
+    }
+
+    @Override
+    public int liters(double time) {
+        return 0;
+    }
+
+    @Override
+    public double getVolumeOfCuboid(double length, double width, double height) {
+        return 0;
+    }
+
+    @Override
+    public float mpgToKPM(float mpg) {
+        return 0;
+    }
+
+    @Override
+    public int[] squareOrSquareRoot(int[] array) {
+        return new int[0];
+    }
+
+    @Override
+    public int[] countPositivesSumNegatives(int[] input) {
+        return new int[0];
+    }
+
+    @Override
+    public int stringToNumber(String str) {
+        return 0;
+    }
+
+    @Override
+    public boolean amIWilson(double n) {
+        if (n < 2) {
+            return false;
+        }
+        double factorial = 1;
+        for (int i = 1; i < n; i++) {
+            factorial *= i;
+        }
+        return ((factorial + 1) / n * n) % 1 == 0;
+    }
+    @Override
+    public double twoDecimalPlaces(double number) {
+        return 0;
+    }
+
+    @Override
+    public int[] divisibleBy(int[] numbers, int divider) {
+        return new int[0];
+    }
+}
