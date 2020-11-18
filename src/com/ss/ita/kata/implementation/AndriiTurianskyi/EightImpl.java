7aa3c51e3d38ffd39fd9d68ba4a55fd1569b63a3
AndriiTurianskyi <andriy.turyansky@gmail.com>
2020-11-18 14:52:48 +0200

    added to the EightImpl class the getVolumeOfCuboid method

---
+++ src/com/ss/ita/kata/implementation/AndriiTurianskyi/EightImpl.java
@@ -0,0 +1,57 @@
+package com.ss.ita.kata.implementation.AndriiTurianskyi;
+
+import com.ss.ita.kata.Eight;
+
+public class EightImpl implements Eight {
+    @Override
+    public int Liters(double time) {
+        return 0;
+    }
+
+    @Override
+    public int liters(double time) {
+        return 0;
+    }
+
+    @Override
+    public double getVolumeOfCuboid(double length, double width, double height) {
+        if (length > 0 && width > 0 && height > 0) {
+            return length * width * height;
+        } else throw new IllegalArgumentException("values can't be less than 0");
+    }
+
+    @Override
+    public float mpgToKPM(float mpg) {
+        return 0;
+    }
+
+    @Override
+    public int[] squareOrSquareRoot(int[] array) {
+        return new int[0];
+    }
+
+    @Override
+    public int[] countPositivesSumNegatives(int[] input) {
+        return new int[0];
+    }
+
+    @Override
+    public int stringToNumber(String str) {
+        return 0;
+    }
+
+    @Override
+    public boolean amIWilson(double n) {
+        return false;
+    }
+
+    @Override
+    public double twoDecimalPlaces(double number) {
+        return 0;
+    }
+
+    @Override
+    public int[] divisibleBy(int[] numbers, int divider) {
+        return new int[0];
+    }
+}
