1cdaa34a1f76eea7066f744007cf38487f265ac3
vadkostuk <vadkostuk@gmail.com>
2020-11-18 16:43:48 +0200

    added solution for task Miles per gallon to kilometers per liter

---
+++ src/com/ss/ita/kata/implementation/vadkostuk/ImplEight.java
@@ -0,0 +1,56 @@
+package com.ss.ita.kata.implementation.vadkostuk;
+
+import com.ss.ita.kata.Eight;
+
+public class ImplEight implements Eight {
+
+    @Override
+    public int Liters(double time) {
+        return 0;
+    }
+
+    @Override
+    public int liters(double time) {
+        return 0;
+    }
+
+    @Override
+    public double getVolumeOfCuboid(double length, double width, double height) {
+        return 0;
+    }
+
+    @Override
+    public float mpgToKPM(float mpg) {
+        return Math.round(mpg * 1.609344f / 4.54609188f);
+    }
+
+    @Override
+    public int[] squareOrSquareRoot(int[] array) {
+        return new int[0];
+    }
+
+    @Override
+    public int[] countPositivesSumNegatives(int[] input) {
+        return new int[0];
+    }
+
+    @Override
+    public int stringToNumber(String str) {
+        return 0;
+    }
+
+    @Override
+    public boolean amIWilson(double n) {
+        return false;
+    }
+
+    @Override
+    public double twoDecimalPlaces(double number) {
+        return 0;
+    }
+
+    @Override
+    public int[] divisibleBy(int[] numbers, int divider) {
+        return new int[0];
+    }
+}
