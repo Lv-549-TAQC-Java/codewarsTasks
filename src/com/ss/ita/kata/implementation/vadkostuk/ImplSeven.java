8d26f7dd7df23bb310a3d3ec34de48508fb9a786
vadkostuk <vadkostuk@gmail.com>
2020-11-18 17:35:25 +0200

    added solution for task Looking for a benefactor

---
+++ src/com/ss/ita/kata/implementation/vadkostuk/ImplSeven.java
@@ -0,0 +1,31 @@
+package com.ss.ita.kata.implementation.vadkostuk;
+
+import com.ss.ita.kata.Seven;
+
+public class ImplSeven implements Seven {
+    @Override
+    public long newAvg(double[] arr, double navg) {
+        int count = 0;
+        double sum = 0;
+        for (double v : arr) {
+            sum = sum + v;
+            count++;
+        }
+        long result = (long) Math.ceil((navg * (count + 1) - sum));
+        if (result > 0) {
+            return result;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Override
+    public String seriesSum(int n) {
+        return null;
+    }
+
+    @Override
+    public int whereIsHe(int p, int bef, int aft) {
+        return 0;
+    }
+}
