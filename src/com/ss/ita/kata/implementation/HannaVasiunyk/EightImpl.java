92fbaa7e4e9dd5b8530155659de42ee55a19f1a5
Hanna Vasiunyk <annvasiunyk@gmail.com>
2020-11-18 14:00:04 +0200

    Added task Count of positives / sum of negatives

---
+++ src/com/ss/ita/kata/implementation/HannaVasiunyk/EightImpl.java
@@ -0,0 +1,62 @@
+package com.ss.ita.kata.implementation.HannaVasiunyk;
+
+import com.ss.ita.kata.Eight;
+
+public class EightImpl implements Eight {
+
+    @Override
+    public int Liters(double time) {
+        return 0;
+    }
+
+    @Override
+    public int liters(double time) {
+        return 0;
+    }
+
+    @Override
+    public double getVolumeOfCuboid(double length, double width, double height) {
+        return 0;
+    }
+
+    @Override
+    public float mpgToKPM(float mpg) {
+        return 0;
+    }
+
+    @Override
+    public int[] squareOrSquareRoot(int[] array) {
+        return new int[0];
+    }
+
+    @Override
+    public int[] countPositivesSumNegatives(int[] input) {
+        int[] array = new int[2];
+        for (int i : input) {
+            if (i > 0) {
+                array[0]+= 1;
+            } else array[1] += i;
+        }
+        return array;
+    }
+
+    @Override
+    public int stringToNumber(String str) {
+        return 0;
+    }
+
+    @Override
+    public boolean amIWilson(double n) {
+        return false;
+    }
+
+    @Override
+    public double twoDecimalPlaces(double number) {
+        return 0;
+    }
+
+    @Override
+    public int[] divisibleBy(int[] numbers, int divider) {
+        return new int[0];
+    }
+}
