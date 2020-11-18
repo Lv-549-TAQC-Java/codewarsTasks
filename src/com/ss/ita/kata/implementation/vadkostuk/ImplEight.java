9ed0ec8e147bd7a8b1061015ccc50adb14cabd1c
vadkostuk <vadkostuk@gmail.com>
2020-11-18 16:58:36 +0200

    added solution for task Count of positives / sum of negatives

---
+++ src/com/ss/ita/kata/implementation/vadkostuk/ImplEight.java
@@ -0,0 +1,67 @@
+package com.ss.ita.kata.implementation.vadkostuk;
+
+import com.ss.ita.kata.Eight;
+
+public class ImplEight implements Eight {
+    @Override
+    public int Liters(double time) {
+        return 0;
+    }
+
+    @Override
+    public int liters(double time) {
+        return 0;
+    }
+
+    @Override
+    public double getVolumeOfCuboid(double length, double width, double height) {
+        return 0;
+    }
+
+    @Override
+    public float mpgToKPM(float mpg) {
+        return 0;
+    }
+
+    @Override
+    public int[] squareOrSquareRoot(int[] array) {
+        return new int[0];
+    }
+
+    @Override
+    public int[] countPositivesSumNegatives(int[] input) {
+        if (input == null || input.length == 0) {
+            return new int[]{};
+        }
+        int count = 0;
+        int sumNeg = 0;
+        for (int j : input) {
+            if (j > 0) {
+                count++;
+            } else if (j < 0) {
+                sumNeg += j;
+            }
+        }
+        return new int[]{count, sumNeg};
+    }
+
+    @Override
+    public int stringToNumber(String str) {
+        return 0;
+    }
+
+    @Override
+    public boolean amIWilson(double n) {
+        return false;
+    }
+
+    @Override
+    public double twoDecimalPlaces(double number) {
+        return 0;
+    }
+
+    @Override
+    public int[] divisibleBy(int[] numbers, int divider) {
+        return new int[0];
+    }
+}
