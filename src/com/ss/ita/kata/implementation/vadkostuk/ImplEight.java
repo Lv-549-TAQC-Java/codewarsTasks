a616a3186137778cb44058f0c6d5924013ad9f9d
vadkostuk <vadkostuk@gmail.com>
2020-11-18 17:29:38 +0200

    added solution for task Find numbers which are divisible by given number

---
+++ src/com/ss/ita/kata/implementation/vadkostuk/ImplEight.java
@@ -0,0 +1,69 @@
+package com.ss.ita.kata.implementation.vadkostuk;
+
+import com.ss.ita.kata.Eight;
+
+import java.util.ArrayList;
+
+public class ImplEight implements Eight {
+    @Override
+    public int Liters(double time) {
+        return 0;
+    }
+
+    @Override
+    public int liters(double time) {
+        return 0;
+    }
+
+    @Override
+    public double getVolumeOfCuboid(double length, double width, double height) {
+        return 0;
+    }
+
+    @Override
+    public float mpgToKPM(float mpg) {
+        return 0;
+    }
+
+    @Override
+    public int[] squareOrSquareRoot(int[] array) {
+        return new int[0];
+    }
+
+    @Override
+    public int[] countPositivesSumNegatives(int[] input) {
+        return new int[0];
+    }
+
+    @Override
+    public int stringToNumber(String str) {
+        return 0;
+    }
+
+    @Override
+    public boolean amIWilson(double n) {
+        return false;
+    }
+
+    @Override
+    public double twoDecimalPlaces(double number) {
+        return 0;
+    }
+
+    @Override
+    public int[] divisibleBy(int[] numbers, int divider) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        for (int number : numbers) {
+            if (number % divider == 0) {
+                arrayList.add(number);
+            }
+        }
+        int[] res = new int[arrayList.size()];
+        for (int i = 0; i < arrayList.size(); i++) {
+            res[i] = arrayList.get(i);
+        }
+
+        return res;
+    }
+}
