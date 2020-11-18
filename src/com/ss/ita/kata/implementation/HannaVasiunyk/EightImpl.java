5006fd806e7a8c8b8eb6a6185833b3b2128a9bbf
Hanna Vasiunyk <annvasiunyk@gmail.com>
2020-11-19 00:38:54 +0200

    conflict resolution

--- src/com/ss/ita/kata/implementation/HannaVasiunyk/EightImpl.java
+++ src/com/ss/ita/kata/implementation/HannaVasiunyk/EightImpl.java
@@ -5,11 +5,56 @@ import com.ss.ita.kata.Eight;
 public class EightImpl implements Eight {
 
     @Override
+    public int Liters(double time) {
+        return 0;
+    }
+
+    @Override
+    public int liters(double time) {
+        return 0;
+    }
+
+    @Override
+    public double getVolumeOfCuboid(double length, double width, double height) {
+        return 0;
+    }
+
+    @Override
     public float mpgToKPM(float mpg) {
         final double imperial = 4.54609188;
         final double km = 1.61;
         float convert =  (mpg / (float)imperial) * (float)km;
         return (Math.round(convert * 100) / 100.f);
+    }
+
+    @Override
+    public int[] squareOrSquareRoot(int[] array) {
+        return new int[0];
+    }
+
+    @Override
+    public int[] countPositivesSumNegatives(int[] input) {
+        return new int[0];
+    }
+
+    @Override
+    public int stringToNumber(String str) {
+        return 0;
+    }
+
+    @Override
+    public boolean amIWilson(double n) {
+        return false;
+    }
+
+    @Override
+    public double twoDecimalPlaces(double number) {
+        return 0;
+    }
+
+    @Override
+    public int[] divisibleBy(int[] numbers, int divider) {
+        return new int[0];
     }
 }
 
