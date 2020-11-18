fc5c08460b433508362419592343ac19c070c2f5
“Anastasia <linka0393@gmail.com>
2020-11-18 19:53:04 +0200

    add impl task keep hydrated

--- src/com/ss/ita/kata/implementation/Nastia3/EightImpl.java
+++ src/com/ss/ita/kata/implementation/Nastia3/EightImpl.java
@@ -5,7 +5,11 @@ import com.ss.ita.kata.Eight;
 public class EightImpl implements Eight {
 
     @Override
-    public int Liters(double time) {
-        return 1;
+
+    public int Liters(double time)  {
+
+        int amount = (int)time /2;
+        return amount;
+
     }
 }
