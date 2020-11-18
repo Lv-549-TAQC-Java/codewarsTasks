f2388f9db4d2be7c4d5a991fa7a51c02bf13f3e0
vadkostuk <vadkostuk@gmail.com>
2020-11-18 16:52:32 +0200

    added solution for task To square(root) or not to square(root)

---
+++ src/com/ss/ita/kata/implementation/vadkostuk/ImplEight.java
@@ -0,0 +1,65 @@
+package com.ss.ita.kata.implementation.vadkostuk;
+
+import com.ss.ita.kata.Eight;
+
+public class ImplEight implements Eight {
+
+    @Override
+    public int Liters(double time) {
+        return 0;
+    }
+
+    @Override
+    public int liters(double time) {
+        return 0;
+    }
+
+    @Override
+    public double getVolumeOfCuboid(double length, double width, double height) {
+        return 0;
+    }
+
+    @Override
+    public float mpgToKPM(float mpg) {
+        return 0;
+    }
+
+    @Override
+    public int[] squareOrSquareRoot(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            {
+                if (Math.sqrt(array[i]) == (int) Math.sqrt(array[i])) {
+                    array[i] = (int) Math.sqrt(array[i]);
+                } else {
+                    array[i] = array[i] * array[i];
+                }
+            }
+        }
+        return array;
+    }
+
+    @Override
+    public int[] countPositivesSumNegatives(int[] input) {
+        return new int[0];
+    }
+
+    @Override
+    public int stringToNumber(String str) {
+        return 0;
+    }
+
+    @Override
+    public boolean amIWilson(double n) {
+        return false;
+    }
+
+    @Override
+    public double twoDecimalPlaces(double number) {
+        return 0;
+    }
+
+    @Override
+    public int[] divisibleBy(int[] numbers, int divider) {
+        return new int[0];
+    }
+}
