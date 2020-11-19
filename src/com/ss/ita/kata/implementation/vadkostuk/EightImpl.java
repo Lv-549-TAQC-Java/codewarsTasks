63c161e89956fdb14ea9454b83a19bea59685e24
vadkostuk <vadkostuk@gmail.com>
2020-11-19 13:07:17 +0200

    renamed class

--- src/com/ss/ita/kata/implementation/vadkostuk/EightImpl.java
+++ src/com/ss/ita/kata/implementation/vadkostuk/EightImpl.java
@@ -2,7 +2,7 @@ package com.ss.ita.kata.implementation.vadkostuk;
 
 import com.ss.ita.kata.Eight;
 
-public class ImplEight implements Eight {
+public class EightImpl implements Eight {
 
     @Override
     public int Liters(double time) {
