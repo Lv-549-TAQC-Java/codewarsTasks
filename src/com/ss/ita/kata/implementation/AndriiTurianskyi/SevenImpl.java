176f714402a3d82ed793113d373c28bdc199a12f
AndriiTurianskyi <andriy.turyansky@gmail.com>
2020-11-20 01:52:52 +0200

    The newAvg method has been added to the SevenImpl class

---
+++ src/com/ss/ita/kata/implementation/AndriiTurianskyi/SevenImpl.java
@@ -0,0 +1,31 @@
+package com.ss.ita.kata.implementation.AndriiTurianskyi;
+
+import com.ss.ita.kata.Seven;
+
+public class SevenImpl implements Seven {
+    @Override
+    public long newAvg(double[] arr, double navg) {
+        if (arr.length == 0) {
+            return (long) Math.ceil(navg);
+        }
+        double sumElem = 0;
+        for (double elem : arr) {
+            sumElem += elem;
+        }
+        double donation = Math.ceil((navg * (arr.length+1)) - sumElem);
+        if (donation > 0) {
+            return (long) donation;
+        } else throw new IllegalArgumentException();
+
+    }
+
+    @Override
+    public String seriesSum(int n) {
+        return null;
+    }
+
+    @Override
+    public int whereIsHe(int p, int bef, int aft) {
+        return 0;
+    }
+}
