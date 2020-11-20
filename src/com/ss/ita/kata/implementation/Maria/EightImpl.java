f5321923675769c4529cdbcd3aa8063f9e381aa5
mariareshetovska <mresetovska@gmail.com>
2020-11-21 00:58:05 +0200

    fixed Liters

--- src/com/ss/ita/kata/implementation/Maria/EightImpl.java
+++ src/com/ss/ita/kata/implementation/Maria/EightImpl.java
@@ -6,9 +6,7 @@ public class EightImpl implements Eight {
 
     @Override
     public int liters(double time) {
-        double letres = time /2;
-        int value = (int)letres;
-        return value;
+        return 0;
     }
 
     @Override
