2d9fd2e2648dfe696ac605f1ccece9f0ccb8743b
mariareshetovska <mresetovska@gmail.com>
2020-11-20 03:13:41 +0200

    impl task Miles per gallon to kilometers per liter

--- src/com/ss/ita/kata/implementation/Maria/EightImpl.java
+++ src/com/ss/ita/kata/implementation/Maria/EightImpl.java
@@ -1,2 +1,57 @@
-package com.ss.ita.kata.implementation.Maria;public class EightImpl {
+package com.ss.ita.kata.implementation.Maria;
+
+import com.ss.ita.kata.Eight;
+
+public class EightImpl implements Eight {
+    @Override
+    public int Liters(double time) {
+        return 0;
+    }
+
+    @Override
+    public int liters(double time) {
+        return 0;
+    }
+
+    @Override
+    public double getVolumeOfCuboid(double length, double width, double height) {
+        return 0;
+    }
+
+    @Override
+    public float mpgToKPM(float mpg) {
+        float kpl = 1.609344f / 4.54609188f * mpg;
+        float n = (float) Math.round(kpl * 1000) / 100;
+        return n;
+    }
+
+    @Override
+    public int[] squareOrSquareRoot(int[] array) {
+        return new int[0];
+    }
+
+    @Override
+    public int[] countPositivesSumNegatives(int[] input) {
+        return new int[0];
+    }
+
+    @Override
+    public int stringToNumber(String str) {
+        return 0;
+    }
+
+    @Override
+    public boolean amIWilson(double n) {
+        return false;
+    }
+
+    @Override
+    public double twoDecimalPlaces(double number) {
+        return 0;
+    }
+
+    @Override
+    public int[] divisibleBy(int[] numbers, int divider) {
+        return new int[0];
+    }
 }
