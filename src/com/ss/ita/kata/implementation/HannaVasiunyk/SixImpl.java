23b0fa25725e1fc3f4bd1dd023edf89c078bd4a4
Hanna Vasiunyk <annvasiunyk@gmail.com>
2020-11-21 17:09:48 +0200

    removed if

--- src/com/ss/ita/kata/implementation/HannaVasiunyk/SixImpl.java
+++ src/com/ss/ita/kata/implementation/HannaVasiunyk/SixImpl.java
@@ -3,6 +3,9 @@ package com.ss.ita.kata.implementation.HannaVasiunyk;
 import com.ss.ita.kata.Six;
 
 public class SixImpl implements Six {
+
+    private static final String NEW_LINE_CHARACTER = "\\r\\n";
+
     @Override
     public long findNb(long m) {
         return 0;
@@ -10,27 +13,24 @@ public class SixImpl implements Six {
 
     @Override
     public String balance(String book) {
-        final String NEW_LINE_CHARACTER = "\\r\\n";
         book = book.replaceAll("[^a-zA-z0-9.\\s]", "");
         String[] lines = book.split("\\n");
         StringBuilder result = new StringBuilder();
-        double balance = 0;
         double totalExpense = 0;
         int expenseCount = 0;
-        for (int i = 0; i < lines.length; i++) {
-            double localBalance = 0;
-            if (i == 0) {
-                result.append("Original Balance: ").append(lines[i]).append(NEW_LINE_CHARACTER);
-                balance = Double.parseDouble(lines[i]);
-            } else {
-                if (!lines[i].isEmpty()) {
-                    String[] records = lines[i].split(" ");
-                    localBalance = balance - Double.parseDouble(records[2]);
-                    balance = localBalance;
-                    result.append(lines[i]).append(" Balance ").append(String.format("%.2f", localBalance)).append(NEW_LINE_CHARACTER);
-                    totalExpense += Double.parseDouble(records[2]);
-                    expenseCount++;
-                }
+
+        result.append("Original Balance: ").append(lines[0]).append(NEW_LINE_CHARACTER);
+        double balance = Double.parseDouble(lines[0]);
+
+        for (int i = 1; i < lines.length; i++) {
+            double localBalance;
+            if (!lines[i].isEmpty()) {
+                String[] records = lines[i].split(" ");
+                localBalance = balance - Double.parseDouble(records[2]);
+                balance = localBalance;
+                result.append(lines[i]).append(" Balance ").append(String.format("%.2f", localBalance)).append(NEW_LINE_CHARACTER);
+                totalExpense += Double.parseDouble(records[2]);
+                expenseCount++;
             }
         }
         result.append("Total expense  ").append(String.format("%.2f", totalExpense)).append(NEW_LINE_CHARACTER);
