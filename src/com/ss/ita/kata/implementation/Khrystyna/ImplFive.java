7700257134351cb0fe2bd76a50799d573268fd17
Khrystyna Pelekh <Khrystyna.Pelekh@outlook.com>
2020-11-23 00:30:35 +0200

    Implement Gap in Primes

---
+++ src/com/ss/ita/kata/implementation/Khrystyna/ImplFive.java
@@ -0,0 +1,60 @@
+package com.ss.ita.kata.implementation.Khrystyna;
+
+import com.ss.ita.kata.Five;
+
+import java.math.BigInteger;
+
+public class ImplFive implements Five {
+    @Override
+    public int artificialRain(int[] v) {
+        return 0;
+    }
+
+    @Override
+    public long[] gap(int g, long m, long n) {
+        for (long i = m; i <= n - g; i++) {
+            if (isPrime(i) && isPrime((i + g))) {
+                boolean hasPrime = false;
+                for (long j = i + 1; j < i + g; j++) {
+                    if (isPrime(j)) {
+                        hasPrime = true;
+                        break;
+                    }
+                }
+                if (!hasPrime) {
+                    return new long[]{i, i + g};
+                }
+            }
+        }
+        return null;
+    }
+
+    public boolean isPrime(long number) {
+        for (long i = 2; i < number / 2; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public int zeros(int n) {
+        return 0;
+    }
+
+    @Override
+    public BigInteger perimeter(BigInteger n) {
+        return null;
+    }
+
+    @Override
+    public double solveSum(double m) {
+        return 0;
+    }
+
+    @Override
+    public long[] smallest(long n) {
+        return new long[0];
+    }
+}
