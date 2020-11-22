0b64f7b657b8b9e99b6df092adac12a24c3743f9
AndriiTurianskyi <andriy.turyansky@gmail.com>
2020-11-23 01:45:02 +0200

    stockSummary methods was added to SixImpl class

--- src/com/ss/ita/kata/implementation/AndriiTurianskyi/SixImpl.java
+++ src/com/ss/ita/kata/implementation/AndriiTurianskyi/SixImpl.java
@@ -3,8 +3,9 @@ package com.ss.ita.kata.implementation.AndriiTurianskyi;
 import com.ss.ita.kata.Six;
 
 import java.math.BigDecimal;
-
 import java.math.MathContext;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SixImpl implements Six {
     @Override
@@ -37,16 +38,16 @@ public class SixImpl implements Six {
         int countForAv = 0;
         for (int i = 3; i < array.length; i += 3) {
             balance -= Double.parseDouble(array[i]);
-            resultStr.append(String.format("\n%s %s %s Balance %.2f", array[i-2], array[i-1], array[i], balance));
+            resultStr.append(String.format("\n%s %s %s Balance %.2f", array[i - 2], array[i - 1], array[i], balance));
             countForAv++;
         }
         double totalSum = Double.parseDouble(array[0]) - balance;
-        return String.format("Original_Balance: %s %s\nTotal expense  %.2f\nAverage expense  %.2f",array[0],resultStr.toString(),totalSum,(totalSum / countForAv));
+        return String.format("Original_Balance: %s %s\nTotal expense  %.2f\nAverage expense  %.2f", array[0], resultStr.toString(), totalSum, (totalSum / countForAv));
     }
 
     @Override
     public double f(double x) {
-        BigDecimal value =new BigDecimal(x);
+        BigDecimal value = new BigDecimal(x);
         return value.add(BigDecimal.ONE).sqrt(MathContext.DECIMAL128).subtract(BigDecimal.ONE).doubleValue();
     }
 
@@ -67,6 +68,22 @@ public class SixImpl implements Six {
 
     @Override
     public String stockSummary(String[] lstOfArt, String[] lstOf1stLetter) {
-        return null;
+        Map<String, Integer> mapResult = new HashMap<>();
+        for (String firsLetter : lstOf1stLetter) {
+            mapResult.put(firsLetter, 0);
+        }
+        for (String artElem : lstOfArt) {
+            String firstLetter = artElem.charAt(0) + "";
+            if (mapResult.containsKey(firstLetter)) {
+                int numbOfArt = Integer.parseInt(artElem.substring(artElem.indexOf(" ") + 1));
+                mapResult.replace(firstLetter, mapResult.get(firstLetter) + numbOfArt);
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        for (Map.Entry entry : mapResult.entrySet()) {
+            result.append(String.format("(%s : %s) - ", entry.getKey(), entry.getValue()));
+        }
+        result.deleteCharAt(result.lastIndexOf("-"));
+        return result.toString().trim();
     }
 }
