996b0fc88918dd90094c16588b75bbb8eb0f57f9
Hanna Vasiunyk <annvasiunyk@gmail.com>
2020-11-22 16:17:36 +0200

    Added task Number of trailing zeros of N!

---
+++ src/com/ss/ita/kata/implementation/HannaVasiunyk/FiveImpl.java
@@ -0,0 +1,42 @@
+package com.ss.ita.kata.implementation.HannaVasiunyk;
+
+import com.ss.ita.kata.Five;
+
+import java.math.BigInteger;
+
+public class FiveImpl implements Five {
+    @Override
+    public int artificialRain(int[] v) {
+        return 0;
+    }
+
+    @Override
+    public long[] gap(int g, long m, long n) {
+        return new long[0];
+    }
+
+    @Override
+    public int zeros(int n) {
+        int zeroSum = 0;
+        while(n > 0){
+            n = n / 5;
+            zeroSum += n;
+        }
+        return zeroSum;
+    }
+
+    @Override
+    public BigInteger perimeter(BigInteger n) {
+        return null;
+    }
+
+    @Override
+    public double solveSum(double m) {
+        return 0;
+    }
+
+    @Override
+    public long[] smallest(long n) {
+        return new long[0];
+    }
+}
