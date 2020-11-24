6d9986320c3b105fcffdcdd1084f22785fb69dbd
93mike <mishakaspar@gmail.com>
2020-11-24 12:10:41 +0200

    [93mike] impl task Scan readIntArray #393

--- src/com/ss/ita/util/implementation/ConsoleScanner.java
+++ src/com/ss/ita/util/implementation/ConsoleScanner.java
@@ -39,7 +39,22 @@ public class ConsoleScanner implements Scanner {
 
     @Override
     public int[] readIntArray() {
-        return new int[0];
+        List<Integer> integers = new ArrayList<>();
+        while (true) {
+            if (scanner.hasNextInt()) {
+                integers.add(scanner.nextInt());
+            } else {
+                scanner.next();
+            }
+
+            if(!scanner.hasNext()) {
+                int[] newInt = new int[integers.size()];
+                for (int i = 0; i < integers.size(); i++) {
+                    newInt[i] = integers.get(i);
+                }
+                return newInt;
+            }
+        }
     }
 
     @Override
