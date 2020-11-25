fd267b4e46d5656f10757fb4cd8d52c46c60fd64
mariareshetovska <mresetovska@gmail.com>
2020-11-25 20:56:38 +0200

    [mariareshetovska] impl task Gap in Primes

--- src/com/ss/ita/kata/implementation/Maria/FiveImpl.java
+++ src/com/ss/ita/kata/implementation/Maria/FiveImpl.java
@@ -27,7 +27,24 @@ public class FiveImpl implements Five {
 
     @Override
     public long[] gap(int g, long m, long n) {
-        return new long[0];
+        long last = Long.MIN_VALUE;
+        for (long i = m; i < n; i++) {
+            if (isPrime(i)) {
+                if (i - last == g) {
+                    return new long[]{last, i};
+                }
+                last = i;
+            }
+        }
+
+        return null;
+    }
+
+    private static boolean isPrime(long i) {
+        for (long j = 2; j < i / 2; j++) {
+            if (i % j == 0) return false;
+        }
+        return true;
     }
 
     @Override
