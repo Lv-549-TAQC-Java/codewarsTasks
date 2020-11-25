60b8995e0ec128eef71f1c9fe0aad2993133cdc7
mariareshetovska <mresetovska@gmail.com>
2020-11-26 00:07:03 +0200

    [mariareshetovska] impl task Trailing zeros in factorial

--- src/com/ss/ita/kata/implementation/Maria/FiveImpl.java
+++ src/com/ss/ita/kata/implementation/Maria/FiveImpl.java
@@ -32,7 +32,9 @@ public class FiveImpl implements Five {
 
     @Override
     public int zeros(int n) {
-        return 0;
+        int result = 0;
+        while (n > 1) result += n /= 5;
+        return result;
     }
 
     @Override
