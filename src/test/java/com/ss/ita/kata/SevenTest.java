ae4b6a7586c82be947429fee61b27c5ba941bb02
93mike <mishakaspar@gmail.com>
2020-11-27 08:56:45 +0200

    maven/mike93

--- src/test/java/com/ss/ita/kata/SevenTest.java
+++ src/test/java/com/ss/ita/kata/SevenTest.java
@@ -1,13 +1,38 @@
 package com.ss.ita.kata;
 
+import com.ss.ita.kata.implementation.mike.SevenImpl;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
 
 public class SevenTest {
+    private SevenImpl sevenMike;
+
+    @BeforeClass
+    public void beforeClass() {
+        sevenMike = new SevenImpl();
+    }
 
     @Test
-    public void testNewAvg() {
+    public void testNewAvg1() {
+        double actualResult = sevenMike.newAvg(new double[] {14.0, 30.0, 5.0, 7.0, 9.0, 11.0, 16.0},90);
+        double expectedResult =628;
+        assertEquals(actualResult, expectedResult);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testNewAvg2() {
+        double actualResult = sevenMike.newAvg( new double[] {14, 30, 5, 7, 9, 11, 15},2);
+        //double expectedResult =628;
+        //assertEquals(actualResult, expectedResult);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testNewAvg3() {
+        double actualResult = sevenMike.newAvg(new double[] {12, 0, 43,2},9);
+        double expectedResult =628;
+        assertEquals(actualResult, expectedResult);
     }
 
     @Test
