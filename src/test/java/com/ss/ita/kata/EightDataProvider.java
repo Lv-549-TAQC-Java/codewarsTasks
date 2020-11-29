ac28f1bf8a61905c30527056f4e85a01d9830d88
vadkostuk <vadkostuk@gmail.com>
2020-11-29 13:48:37 +0200

    update branch

---
+++ src/test/java/com/ss/ita/kata/EightDataProvider.java
@@ -0,0 +1,13 @@
+package com.ss.ita.kata;
+
+import org.testng.annotations.DataProvider;
+
+public class EightDataProvider extends DataProviderClass{
+    @DataProvider(name = "validMpgToKpmDataProvider")
+    public static Object[][] validMpgToKpmDataProvider(){
+        Object[][] param = new Object[][]{
+                {10,3.54f},{20,7.08f}};
+
+        return getObjectCodes(validEightImplPackageProvider(),param);
+    }
+}
