72813720054086108dfaf34393a8c10869c0fed0
AndriiTurianskyi <andriy.turyansky@gmail.com>
2020-11-29 21:28:12 +0200

    added test for f method

--- src/test/java/com/ss/ita/kata/SixDataProvider.java
+++ src/test/java/com/ss/ita/kata/SixDataProvider.java
@@ -1,5 +1,16 @@
 package com.ss.ita.kata;
 
+import org.testng.annotations.DataProvider;
+
 public class SixDataProvider extends DataProviderClass{
+    @DataProvider
+    public Object[][] dataForF() {
+        Object[][] param = new Object[][]{
+                {2.6e-08,1.29999999155e-08},
+                {1.4e-09,6.999999997549999e-10},
+                {5.0e-06,2.499996875007812e-06}
+        };
+        return getObjectCodes(validSixImplPackageProvider(),param);
+    }
 
 }
