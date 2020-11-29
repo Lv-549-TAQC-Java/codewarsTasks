b14b0edfd50ebffd90612af97e00ab86a400f21d
AndriiTurianskyi <andriy.turyansky@gmail.com>
2020-11-29 21:18:39 +0200

    added test for task amIWilson

--- src/test/java/com/ss/ita/kata/EightDataProvider.java
+++ src/test/java/com/ss/ita/kata/EightDataProvider.java
@@ -10,4 +10,18 @@ public class EightDataProvider extends DataProviderClass{
 
         return getObjectCodes(validEightImplPackageProvider(),param);
     }
+    @DataProvider
+    public Object[][] dataForWilsonNumber() {
+        Object[][] param = new Object[][]{
+                {7, false},
+                {3,false},
+                {0,false},
+                {-5,false},
+                {13,true},
+                {4,false},
+                {3.14,false}
+        };
+        return getObjectCodes(validEightImplPackageProvider(),param);
+    }
+
 }
